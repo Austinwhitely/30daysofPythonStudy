01374b7499c2ecc17351dd8cd33ee2b8806ae664
Austinwhitely <66478382+Austinwhitely@users.noreply.github.com>
2026-06-25 13:28:01 -0400

    Add files via upload
    Completed modules practice problems

---
+++ Modules.py
@@ -0,0 +1,50 @@
+from random import *
+import string
+
+characters: str = string.ascii_letters + string.digits
+empty_array: list = {}
+
+#type: str = input('enter color type: ')
+#num: int = int(input('enter number of colors: '))
+
+#generate_colors(type, num)
+#rgb_color_gen()
+#random_user_id()
+
+def random_user_id():
+
+    len: int = int(input('enter user id length: '))
+    num: int = int(input('enter number of user ids: '))
+ 
+    for number in range(num): 
+         return ''.join(choice(characters) for _ in range(len))
+    
+
+def rgb_color_gen():
+    color: int = randint(0, 255)
+    color1: int = randint(0, 255)
+    color2: int = randint(0, 255)
+    rgb: tuple = (color, color1, color2)
+    print('rgb{rgb}')
+
+def generate_colors(type: str, num: int):
+    
+    if type.lower() == 'rgb':
+        for number in range(num): 
+           
+             rgb: list = [randint(0,255) for i in range(3)]
+             print("rgb({}, {}, {}) " .format(*rgb))
+
+    elif type.lower() == 'hex':
+        for number in range(num): 
+           
+             hex: list = [''.join(choice(characters) for i in range (6))]
+             print("#{} " .format(*hex))        
+             
+             
+def shuffle_list(array: list):
+       array: list = [randint(0,9) for i in range(7)]
+       return array
+
+print(shuffle_list(empty_array))
+     
